d82aba96a21c4dbab5336a2432cfca132efdbcba
LeonardoBFernandes <leonardo.fernandes@sptech.school>
2025-06-05 12:21:46 -0300

    Adicionando botão de atualizar melhor tentativa no quiz e alterando query de login

--- Site/src/models/quizModel.js
+++ Site/src/models/quizModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config")
 
 function listar(id) {
     var instrucao = `
-        SELECT max(pontuacao) as pontuacao FROM tentativaQuiz where fk_idusuario = ('${id}');
+        SELECT max(pontuacao) as pontuacao FROM tentativaQuiz where fk_idusuario = '${id}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
--- Site/src/models/usuarioModel.js
+++ Site/src/models/usuarioModel.js
@@ -3,7 +3,10 @@ var database = require("../database/config")
 function autenticar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        SELECT idUsuario, nome, email FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUsuario, nome, email, max(pontuacao) as pontuacao_maxima FROM usuario usu
+        LEFT JOIN tentativaQuiz tqz on tqz.fk_idusuario = usu.idUsuario
+        WHERE email = '${email}' AND senha = '${senha}'
+        GROUP BY (idUsuario);
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
